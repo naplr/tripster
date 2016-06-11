44187081dd423f991f87c6e260dc5b2a2fa4d23b
Napol Rachatasumrit <naplr42@gmail.com>
2016-06-12 03:32:24 +0700

    Add interim page

---
+++ tripster/tripster/InterimViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InterimViewController.swift
+//  tripster
+//
+//  Created by Napol Rachatasumrit on 6/12/16.
+//  Copyright © 2016 Napol Rachatasumrit. All rights reserved.
+//
+
+import UIKit
+
+class InterimViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
