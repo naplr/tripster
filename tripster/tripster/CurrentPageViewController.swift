b079de5cff7c99903a435e3730300d8ba29789fd
Tadpol Rachatasumrit <tadpol@DecsMBA.local>
2016-06-12 11:03:00 +0700

    UI FIX LATEST

---
+++ tripster/tripster/CurrentPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CurrentPageViewController.swift
+//  tripster
+//
+//  Created by Tadpol Rachatasumrit on 6/12/16.
+//  Copyright © 2016 Napol Rachatasumrit. All rights reserved.
+//
+
+import UIKit
+
+class CurrentPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "page4")!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
